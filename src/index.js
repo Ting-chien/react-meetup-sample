3fad3ad037af17a20dc51995cc9c29f63627e1e2
Eric Wang <eric.wang@theomnieyes.com>
2024-04-05 12:48:01 +0800

    Add navigation and css module

--- src/index.js
+++ src/index.js
@@ -2,19 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+// Pages
 import './index.css';
 import AllMeetupPage from './pages/AllMeetup';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupPage from './pages/NewMeetup';
 
+// Components
+import MainNavigation from './components/MainNavigation';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <h1>Hello Meetup App</h1>
-    <Routes>
-      <Route path='/' element={<AllMeetupPage />} />
-      <Route path='/new' element={<NewMeetupPage />} />
-      <Route path='/favorites' element={<FavoritesPage />} />
-    </Routes>
+      <MainNavigation />
+      <Routes>
+        <Route path='/' element={<AllMeetupPage />} />
+        <Route path='/new' element={<NewMeetupPage />} />
+        <Route path='/favorites' element={<FavoritesPage />} />
+      </Routes>
   </BrowserRouter>
 );
